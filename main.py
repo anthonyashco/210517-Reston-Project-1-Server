11b4762c2fe6500c01ba4c78ba2b5cd2fa517bd2
Anthony Co <anthonyashco@gmail.com>
2021-06-07 12:04:59 -0700

    Push example form handling.

---
+++ main.py
@@ -0,0 +1,16 @@
+"""Main program script."""
+
+import logging
+
+from flask import Flask
+from routes import routes
+
+app = Flask(__name__, static_url_path="/static")
+logging.basicConfig(
+    filename="log.log",
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s")
+routes.create_routes(app)
+
+if __name__ == "__main__":
+    app.run()
